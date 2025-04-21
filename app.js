057f2e5121536d05a89ab96832671418da4e7bca
KYIVKIT <kyivkit88@gmail.com>
2025-04-21 12:38:04 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-
 let maskIndex = 1;
 let tattooIndex = 1;
-const maxMasks = 8;
-const maxTattoos = 9;
 let tattooSettings = {};
 
-fetch("tattoo-settings.json")
-  .then(response => response.json())
-  .then(data => {
-    tattooSettings = data;
-    applyTattooSettings();
-  });
+function loadSettings() {
+  fetch('tattoo-settings.json')
+    .then(res => res.json())
+    .then(data => {
+      tattooSettings = data;
+      updateDisplay();
+    });
+}
 
 function change(type, delta) {
-  if (type === "mask") {
-    maskIndex = (maskIndex + delta + maxMasks - 1) % maxMasks + 1;
-    document.getElementById("baseMask").src = `mask/${maskIndex}.png`;
-  } else if (type === "tattoo") {
-    tattooIndex = (tattooIndex + delta + maxTattoos - 1) % maxTattoos + 1;
-    document.getElementById("tattoo").src = `tattoo/${tattooIndex}.png`;
-    applyTattooSettings();
+  if (type === 'mask') {
+    maskIndex += delta;
+    if (maskIndex < 1) maskIndex = 1;
+    if (maskIndex > 9) maskIndex = 9;
+    document.getElementById('baseMask').src = `mask/${maskIndex}.png`;
+  } else if (type === 'tattoo') {
+    tattooIndex += delta;
+    if (tattooIndex < 1) tattooIndex = 1;
+    if (tattooIndex > 9) tattooIndex = 9;
+    updateDisplay();
   }
 }
 
-function applyTattooSettings() {
-  const tattoo = document.getElementById("tattoo");
-  const setting = tattooSettings[tattooIndex];
-  if (!setting) return;
-  tattoo.style.top = setting.top;
-  tattoo.style.left = setting.left;
-  tattoo.style.width = setting.width;
-  tattoo.style.filter = setting.filter;
+function updateDisplay() {
+  const tattooImg = document.getElementById('tattoo');
+  tattooImg.src = `tattoo/${tattooIndex}.png`;
+  const s = tattooSettings[tattooIndex];
+  if (s) {
+    tattooImg.style.transform = `translate(${s.x}px, ${s.y}px) scale(${s.scale})`;
+    tattooImg.style.filter = s.color === 'black' ? 'brightness(0)' : s.color === 'white' ? 'brightness(10)' : '';
+  }
 }
+
+window.onload = loadSettings;
