3de6bd4a499f9c0aa1edbb02a18e8cd62dd7f967
KYIVKIT <kyivkit88@gmail.com>
2025-04-21 12:53:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ let state = {
   mask: 1,
   tattoo: 1,
   maxMask: 8,
-  maxTattoo: 9,
+  maxTattoo: 9
 };
 
-function change(type, dir) {
-  state[type] += dir;
-  const max = state[type === "mask" ? "maxMask" : "maxTattoo"];
-  if (state[type] > max) state[type] = 1;
-  if (state[type] < 1) state[type] = max;
-  document.getElementById(type === "mask" ? "baseMask" : "tattoo").src = type + "/" + state[type] + ".png";
+function change(type, direction) {
+  state[type] += direction;
+
+  if (state[type] > state["max" + capitalize(type)]) {
+    state[type] = 1;
+  } else if (state[type] < 1) {
+    state[type] = state["max" + capitalize(type)];
+  }
+
+  document.getElementById(type === "mask" ? "baseMask" : "tattoo").src =
+    type + "/" + state[type] + ".png";
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
